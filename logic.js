566ded689a688ec7d274061d708b8dc2f1b16799
corinnachan <corinnaccm@gmail.com>
2019-04-17 14:06:17 +0100

    added functions after TDD on local

--- logic.js
+++ logic.js
@@ -2,6 +2,7 @@
 // you can access these on todo.todoFunctions
 // For part one we expect you to use tdd
 
+var order;
 var todoFunctions = {
   // todoFunctions.generateId() will give you a unique id
   // You do not need to understand the implementation of this function.
@@ -14,44 +15,74 @@ var todoFunctions = {
 
     return incrementCounter;
   })(),
-  
-  //cloneArrayOfObjects will create a copy of the todos array 
+
+  //cloneArrayOfObjects will create a copy of the todos array
   //changes to the new array don't affect the original
   cloneArrayOfObjects: function(todos) {
     return todos.map(function(todo){
       return JSON.parse(JSON.stringify(todo));
     });
   },
-  
+
   addTodo: function(todos, newTodo) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // returns a new array, it should contain todos with the newTodo added to the end.
     // add an id to the newTodo. You can use the generateId function to create an id.
     // hint: array.concat
+    var newTodo = {id: todoFunctions.generateId(), description: newTodo, done: false};
+    return this.cloneArrayOfObjects(todos).concat(newTodo);
   },
+
   deleteTodo: function(todos, idToDelete) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // return a new array, this should not contain any todo with an id of idToDelete
     // hint: array.filter
+    return this.cloneArrayOfObjects(todos).filter( x => todos[x] !== idToDelete);
   },
   markTodo: function(todos, idToMark) {
     // should leave the input argument todos unchanged (you can use cloneArrayOfObjects)
     // in the new todo array, all elements will remain unchanged except the one with id: idToMark
     // this element will have its done value toggled
     // hint: array.map
+    return this.cloneArrayOfObjects(todos).map( x => {
+      if (x.id == idToMark && x.done == false) {
+        return {...x, done: true}
+      }
+      else if (x.id == idToMark && x.done == true) {
+        return {...x, done: false}
+      }
+      else {
+        return {...x, done: false}
+      }
+    })
   },
+  sortFunction: function(todos) {
+    order = !order;
+    return this.cloneArrayOfObjects(todos).sort(function (a,b) {
+      if (order) {
+        return b.id - a.id;
+      } else {
+        return a.id - b.id;
+      };
+
+    });
+  },
+
   sortTodos: function(todos, sortFunction) {
     // stretch goal! Do this last
     // should leave the input arguement todos unchanged (you can use cloneArrayOfObjects)
     // sortFunction will have same signature as the sort function in array.sort
     // hint: array.slice, array.sort
+    var sortedTodos = this.sortFunction(todos);
+    return this.cloneArrayOfObjects(sortedTodos);
+
   },
 };
 
 
 // Why is this if statement necessary?
 // The answer has something to do with needing to run code both in the browser and in Node.js
-// See this article for more details: 
+// See this article for more details:
 // http://www.matteoagosti.com/blog/2013/02/24/writing-javascript-modules-for-both-browser-and-node/
 if (typeof module !== 'undefined') {
   module.exports = todoFunctions;
--- test.js
+++ test.js
@@ -1,7 +1,50 @@
 var test = require('tape');
-var logic = require('./logic');
+var logic = require('./logic.js');
+
 
 test('Example test', function(t) {
   t.pass();
   t.end();
 });
+
+test('Clone array of objects', function(t){
+  var actual = logic.cloneArrayOfObjects([{}]);
+  var expected = [{}];
+  t.deepEquals(actual, expected, 'Clone array should be the same as original array');
+  t.end();
+})
+
+test('Add todo item', function(t){
+  var actual = logic.addTodo([],'task1');
+  var expected = [{id: 1, description: 'task1', done: false}];
+  t.deepEquals(actual, expected, 'task1 should be added to todo array');
+  t.end();
+})
+
+test('Delete todo item', function(t){
+  var actual = logic.deleteTodo([],'1');
+  var expected = [];
+  t.deepEquals(actual,expected, 'ID 1 entry should be deleted');
+  t.end();
+})
+
+test('Mark todo item as done - false to true', function(t){
+  var actual = logic.markTodo([{id: 1, description: 'task1', done: false}], 1);
+  var expected = [{id: 1, description: 'task1', done: true}];
+  t.deepEquals(actual, expected, 'task id 1 marked as done');
+  t.end();
+})
+
+test('Mark todo item as done - true to false', function(t){
+  var actual = logic.markTodo([{id: 1, description: 'task1', done: true}], 1);
+  var expected = [{id: 1, description: 'task1', done: false}];
+  t.deepEquals(actual, expected, 'task id 1 marked as done');
+  t.end();
+})
+
+test('Sort todo items', function(t){
+  var actual = logic.sortTodos([{id:1 }, {id: 2}, {id: 3}]);
+  var expected = ([{id: 3}, {id: 2}, {id: 1}]);
+  t.deepEquals(actual, expected, 'Reverse to do ID');
+  t.end();
+})
